8c3a47da5f32b54da0ae6f990f126411ea687743
delia-pixel <tpatriciareinedia@gmail.com>
2026-05-31 20:16:18 -0400

    parent child communication

--- src/app/products/products-card/products-card.ts
+++ src/app/products/products-card/products-card.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Product } from '../product';
 @Component({
   selector: 'app-products-card',
   imports: [MatButtonModule, MatCardModule],
   templateUrl: './products-card.html',
   styleUrl: './products-card.scss',
 })
-export class ProductsCard {}
+export class ProductsCard {
+  readonly product = input.required<Product>();
+  readonly addButtonLabel = input('Add to cart');
+}
