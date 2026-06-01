77e17a1d62b3f51dd112d3e58946a376bb15a2a5
delia-pixel <tpatriciareinedia@gmail.com>
2026-05-31 20:02:00 -0400

    signals and control flow blocks

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductsCard } from '../products-card/products-card';
+import { Product } from '../product';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductsCard],
+  imports: [ProductsCard, MatIcon],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
-export class ProductsGrid {}
+export class ProductsGrid {
+  protected readonly products = signal<Product[]>([
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'Description for product 1',
+      price: 199.99,
+      originalPrice: 249.99,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'Description for product 2',
+      price: 299.99,
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      description: 'Description for product 3',
+      price: 79.99,
+    },
+  ]);
+}
